1674beb7d6743f2d47fb6da9634f9bedf4f66f69
the-fool <sketchbang@gmail.com>
2016-04-01 07:52:30 -0500

    add axis definitions

--- src/chart.js
+++ src/chart.js
@@ -32,6 +32,8 @@
 
       var width = 600, height 300;
 
+      return postLink;
+
       function postLink(scope, element, attrs)
       {
         scope.$watch('data', function(newVal, oldVal, scope) {
@@ -39,12 +41,45 @@
         }, true);
       }
 
-      return postLink;
-    }
+      function draw(svg, width, height, data)
+      {
+        svg
+          .attr('width', width)
+          .attr('height', height);
+        var margin = 30;
 
-    function draw(svg, width, height, data)
-    {
+        var xScale = d3.time.scale()
+          .domain([
+            d3.min(data, function(d) { return d.time; }),
+            d3.max(data, function(d) { return d.time; })
+          ])
+          .range([margin, width - margin]);
 
+        var xAxis = d3.svg.axis()
+          .scale(xScale)
+          .orient('top')
+          .tickFormat(d3.time.format('%S'));
+
+        var yScale = d3.time.scale()
+          .domain([
+            0,
+            d3.max(data,function(d) {return d.visitors; })
+          ])
+          .range([margin, height - margin]);
+
+        var yAxis = d3.svg.axis()
+          .scale(yScale)
+          .orient('left')
+          .tickFormat(d3.format('f'));
+
+        svg.select('.x-axis')
+          .attr("transform", "translate(0, " + margin + ")")
+          .call(xAxis);
+        svg.select('.y-axis')
+          .attr("transform", "translate(" + margin + ")")
+          .call(yAxis);
+
+      }
     }
   }
 
